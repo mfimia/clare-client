f10503a780569b96d50e678f0d589c105ae75598
Martin Fimia <martin.fimia.osuna@hotmail.com>
2022-02-01 21:46:03 +0100

    helper code

---
+++ src/utils/inputFields.js
@@ -0,0 +1,44 @@
+const inputFields = [
+  {
+    name: "first_name",
+    value: "",
+    error: {
+      value: false,
+      message: "",
+    },
+    label: "First name",
+    valid: false,
+  },
+  {
+    name: "last_name",
+    value: "",
+    error: {
+      value: false,
+      message: "",
+    },
+    label: "Last name",
+    valid: false,
+  },
+  {
+    name: "email",
+    value: "",
+    error: {
+      value: false,
+      message: "",
+    },
+    label: "Email",
+    valid: false,
+  },
+  {
+    name: "referred_by",
+    value: "",
+    error: {
+      value: false,
+      message: "",
+    },
+    label: "Referral code",
+    valid: false,
+  },
+];
+
+export default inputFields;
